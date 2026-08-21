05c8ddc47e1152e5d4690f94588854a5c31438a8
bpcortijo <bpcortijo@gmail.com>
2026-08-21 00:22:15 -0400

    Roulette Wins
    You can now Win or lose Roulette

--- MagicalGirlJamProj/Assets/TwinStick_Prototype/Scripts/RouletteScript.cs
+++ MagicalGirlJamProj/Assets/TwinStick_Prototype/Scripts/RouletteScript.cs
@@ -1,20 +1,22 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class RouletteScript : GamblingScript
 {
+	int pocketIndex;
 	float currentSpin;
 	public float spinSpeed;
 	List<Transform> pockets;
 
-    void Start()
-    {
+	void Start()
+	{
 
-    }
+	}
 
-    public override void Activate(GameObject player)
-    {
+	public override void Activate(GameObject player)
+	{
 		base.Activate(player);
 
 		if (gambleView == null)
@@ -22,7 +24,7 @@ public class RouletteScript : GamblingScript
 			gambleView = Instantiate(uiPrefab, panel);
 			gambleView = gambleView.transform.GetChild(0).gameObject;
 			gambleView.transform.eulerAngles = new Vector3(0, 0, Random.Range(0, 18) * 20);
-			
+
 			pockets = new List<Transform>();
 
 			foreach (Transform child in gambleView.transform)
@@ -40,23 +42,35 @@ public class RouletteScript : GamblingScript
 		while (currentSpin > .5f)
 		{
 			gambleView.transform.eulerAngles += new Vector3(0, 0, currentSpin * Time.deltaTime);
-			spinTime+=Time.deltaTime;
+			spinTime += Time.deltaTime;
 			await Task.Yield();
 
 			if (spinTime > 3)
-				currentSpin -= currentSpin*Time.deltaTime;
+				currentSpin -= currentSpin * Time.deltaTime;
 		}
 
 		spinTime = 0;
 		float angle = gambleView.transform.eulerAngles.z % 360;
-		float finalAngle = Mathf.CeilToInt(angle / 360 * pockets.Count)*(360 / pockets.Count);
+		pocketIndex = Mathf.CeilToInt(angle / 360 * pockets.Count);
+		float finalAngle = pocketIndex * (360 / pockets.Count);
 
 		while (spinTime < 1f)
 		{
-			gambleView.transform.eulerAngles = new Vector3(0, 0, Mathf.LerpAngle(angle, finalAngle, Time.deltaTime*100));
+			gambleView.transform.eulerAngles = new Vector3(0, 0, Mathf.LerpAngle(angle, finalAngle, Time.deltaTime * 100));
 			spinTime += Time.deltaTime;
 		}
 
 		inProgress = false;
+		CheckRewards();
+	}
+
+	void CheckRewards()
+	{
+		string pocketName = pockets[pocketIndex].name;
+		if (pocketName.Contains("B"))
+			Win(Rarity.common);
+		else if (pocketName.Contains("G"))
+			Win(Rarity.epic);
+		else Loss();
 	}
 }
