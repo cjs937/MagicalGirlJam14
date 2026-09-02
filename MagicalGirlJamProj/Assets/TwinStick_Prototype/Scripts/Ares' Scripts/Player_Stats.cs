80ce231c1a8fa25a9a1ead5c2b5932f07e5a5ae4
Mohammad Haider <arzhdr@gmail.com>
2026-09-02 17:47:12 -0400

    Added Timer and health Bar
    * Functioning Timer added on screen
    * Health bar added on top left of screen, (needs to be tested with enemy and projectile collisions)

--- MagicalGirlJamProj/Assets/TwinStick_Prototype/Scripts/Ares' Scripts/Player_Stats.cs
+++ MagicalGirlJamProj/Assets/TwinStick_Prototype/Scripts/Ares' Scripts/Player_Stats.cs
@@ -1,17 +1,29 @@
+using System;
 using UnityEngine;
 
 public class Player_Stats : MonoBehaviour
 {
     public float HP, MaxHP;
+
+    [SerializeField]
+    private HP_UI healthBar;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        healthBar.SetMax_HP(MaxHP);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void SetHealth( float healthChange)
+    {
+        HP += healthChange;
+        HP = Mathf.Clamp(HP, 0, MaxHP);
+
+        healthBar.SetHealth(HP);
     }
 }
---
+++ MagicalGirlJamProj/Assets/TwinStick_Prototype/Scripts/Ares' Scripts/TimerScript.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using TMPro;
+
+public class TimerScript : MonoBehaviour
+{
+    public float time;
+    public TextMeshProUGUI timerText;
+
+    void Update()
+    {
+        time += Time.deltaTime;
+        timerText.text = Mathf.Floor(time).ToString();
+    }
+}
