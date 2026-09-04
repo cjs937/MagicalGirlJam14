2f13f4e35dc1da526a20d9fe21a23695bda39daa
cjs937 <31425502+cjs937@users.noreply.github.com>
2026-09-03 22:30:23 -0400

    Added main scene to build settings and made title screen path to it

--- MagicalGirlJamProj/Assets/TwinStick_Prototype/Scripts/GameState/TitleScreen.cs
+++ MagicalGirlJamProj/Assets/TwinStick_Prototype/Scripts/GameState/TitleScreen.cs
@@ -19,7 +19,7 @@ public class TitleScreen : MonoBehaviour
 
     public void StartGame()
     {
-        SceneManager.LoadScene("BasicScene");
+        SceneManager.LoadScene("MainScene");
     }
 
     public void QuitGame()
